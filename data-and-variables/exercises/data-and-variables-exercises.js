980adb78fff9c3a99c15fa4d73be6fa1fdd337d8
johnkjrfc <johnkjrfc@gmail.com>
2023-11-13 14:18:17 -0600

    worked with Ch. 4 exercises

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,24 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500
+let distMarsKm = 225000000
+let distMoonKm = 384400
+const miPerKm = 0.621
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distMarsKm);
+console.log(typeof distMoonKm);
+console.log(typeof miPerKm);
+
+let distMarsMi = distMarsKm * miPerKm;
+console.log(distMarsMi);
+let hoursToMars = distMarsMi / shuttleSpeedMph;
+console.log(hoursToMars)
+let daysToMars = hoursToMars / 24
+console.log(daysToMars)
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars");
