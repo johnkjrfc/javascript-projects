e471ad90c30d54b0f9fa53ae8c87f8110a261c72
johnkjrfc <johnkjrfc@gmail.com>
2023-11-16 08:56:33 -0600

    worked on exercises

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -2,6 +2,19 @@
 
 // Part One: Reverse Characters
 
+function reverseCharacters(myWord) {
+    let reversedWord;
+
+    if (typeof myWord === 'string') {
+        reversedWord = myWord.split('').reverse().join('');
+    } else {
+        reversedWord = myWord.toString().split('').reverse().join('');
+    }
+    return reversedWord;
+}
+console.log(reverseCharacters('floatation'));
+console.log(reverseCharacters(1692));
+
 // 1. Define the function as reverseCharacters. Give it one parameter, which will be the string to reverse.
 // 2. Within the function, split the string into an array, then reverse the array.
 // 3. Use join to create the reversed string and return that string from the function.
@@ -11,6 +24,7 @@
 
 // Part Two: Reverse Digits
 
+
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
 // 2. If typeof is ‘string’, return the reversed string as before.
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
@@ -18,7 +32,7 @@
 // 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
 
 // Part Three: Complete Reversal
-
+arr = []
 // 1. Define and initialize an empty array.
 // 2. Loop through the old array.
 // 3. For each element in the old array, call reverseCharacters to flip the characters or digits.
